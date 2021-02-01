a82edc2cc2ff95cf804b4f8f1e4785dc207c3d47
vinothkumarnair <vinothkumarnair@gmail.com>
2021-02-01 19:00:24 +0530

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,62 @@
+class Main {  
+
+
+  public static void main(String args[]) {
+    String givenWordToReverse ="vinoth kum";
+    
+    Queue queue=new Queue();
+    
+    char[] wordInChars = givenWordToReverse.toCharArray();
+    
+    for(char charInWord : wordInChars)
+    {
+        queue.insert(charInWord);
+    }
+    
+    for(int i=0;i<10;i++)
+    {
+        System.out.println("char in queue : " + queue.remove());
+    }
+
+  } 
+
+}
+
+class Queue {
+    
+private int index = 0;
+private  char[] queueArr;
+
+public Queue()
+{
+    queueArr = new char[10];
+}
+  public  void insert(char item)
+  {
+      queueArr[index]=item;
+      index=index+1;
+  }
+  
+  public char remove()
+  {
+      char[] newArray = new  char[10];
+      char firstItem = queueArr[0];
+      
+      for(int charIndex=0;charIndex<10;charIndex++)
+      {
+          if(charIndex!=0)
+            newArray[charIndex-1] =  queueArr[charIndex];
+      }
+      queueArr = newArray;
+      
+      return firstItem;
+  }
+
+  public char peek()
+  {
+      return queueArr[0];
+  }
+  
+  //todo : empty check
+}
+
